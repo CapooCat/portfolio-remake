9e6457ce67a9ff611a2d8f9161e7f1aff221f64c
CAPOOCAT-LAPTOP\trant <tranthaitat99@gmail.com>
2024-10-27 20:31:50 +0700

    #Dev + rewrite review

--- src/mocks/reviews.tsx
+++ src/mocks/reviews.tsx
@@ -3,13 +3,13 @@ const reviews = [
     name: "Tuan Truong",
     role: "Frontend Techlead at ByteTech",
     description:
-      "I'm impressed by how quickly Dat learns and adapts to our CDP React projects, his problems solving skill is no exception. He's the guy that we can trust.",
+      "I'm impressed by how quickly Dat learns and adapts to our CDP React projects, his problems solving abilities are no exception. He's the guy that we can trust.",
   },
   {
     name: "Nghi Dao",
     role: "Junior Frontend Developer at ByteTech",
     description:
-      "I love how he make complex logic in React look so simple, making future maintenance and updates feel effortless, we couldn't have done it without him.",
+      "I love how he make complex logic in React look so simple, making future maintenance and updates feel effortless, I'm so glad that I was able to work with him.",
   },
   {
     name: "Thanh Ngoc",
